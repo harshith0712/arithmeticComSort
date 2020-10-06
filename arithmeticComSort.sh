956d6cfecafd8e18dcb71e109aafe4b907f6130d
harshith0712 <kolagatlaharshith@gmail.com>
2020-10-06 20:27:59 +0530

    acending sort

--- arithmeticComSort.sh
+++ arithmeticComSort.sh
@@ -1,36 +1,16 @@
 #!/bin/bash
 
-echo "enter the values of a, b, c"
-read a
-read b
-read c
 
-uc1=$(($a+$b*$c))
-
-echo $uc1
-
-uc2=$(($a*$b+$c))
-
-echo $uc2
-
-uc3=$(($c+$a/$b))
-
-echo $uc3
-
-uc4=$(($a%$b+$c))
-
-echo $uc4
-
-key=0
-dictionary[key++]=$uc1
-dictionary[key++]=$uc2
-dictionary[key++]=$uc3
-dictionary[key++]=$uc4
-
-
-for((key=0; key<=${#dic[@]}; key++))
-do
-array[key]=${dic[key]}
-done
+	for((i=0; i<4; i++))
+   do
+      for((j=0; j<i-1; j++))
+      do
+         if [ ${array[j]} -gt ${array[$((j+1))]} ]
+         then
+            temp=${array[$j]}
+            array[j]=${array[$((j+1))]}
+            array[$((j+1))]=$temp
+         fi
+      done
+   done
 echo "${array[@]}"
-
