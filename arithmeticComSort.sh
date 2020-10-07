75428bf4bf816b3e2bcfa028f56268b6458eef46
harshith0712 <kolagatlaharshith@gmail.com>
2020-10-07 12:39:01 +0530

    usecase5

--- arithmeticComSort.sh
+++ arithmeticComSort.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-key=0
-dictionary[key++]=$uc1
-dictionary[key++]=$uc2
-dictionary[key++]=$uc3
-dictionary[key++]=$uc4
+declare -A dictionary
+
+dictionary[1]=$uc1
+dictionary[2]=$uc2
+dictionary[3]=$uc3
+dictionary[4]=$uc4
